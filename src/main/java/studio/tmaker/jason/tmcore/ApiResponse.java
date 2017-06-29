1d4c1a83b687e1f29327c247ab396099aa33ec97
Jason Tsai <funnot404@gmail.com>
2017-06-29 16:19:39 +0800

    [New Feature] (API) > Response JSON to Object / Array

---
+++ src/main/java/studio/tmaker/jason/tmcore/ApiResponse.java
@@ -0,0 +1,11 @@
+package studio.tmaker.jason.tmcore;
+
+/**
+ * Created by jasontsai on 2017/6/28.
+ */
+
+public class ApiResponse<T> {
+    public String status;
+    public String error;
+    public T data;
+}
--- src/main/java/studio/tmaker/jason/tmcore/TMCore.java
+++ src/main/java/studio/tmaker/jason/tmcore/TMCore.java
@@ -1,4 +1,7 @@
 package studio.tmaker.jason.tmcore;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,17 +12,24 @@ import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.service.media.MediaBrowserService;
+import android.support.v4.media.MediaBrowserServiceCompat;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
 
 /**
  * Created by jasontsai on 2017/6/13.
  */
 
 public class TMCore {
+    Gson gson = new Gson();
     private static TMCore instance = null;
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
@@ -40,17 +50,38 @@ public class TMCore {
         return matcher.find();
     }
 
-    public <T> T convertJsonString(String string, Class<T> tClass) {
+    public <T> T converJson(String string, Class<T> tClass) {
         if (isJSONValid(string)) {
-            JsonParser parser = new JsonParser();
-            JsonElement mJson =  parser.parse(string);
-            Gson gson = new Gson();
-            T o = gson.fromJson(mJson, tClass);
-            return o;
+            return gson.fromJson(string, tClass);
         }else {
             return null;
         }
+    }
 
+    public <T> ApiResponse<T> convertJson2ApiResponse(String string, Class<T> tClass) {
+        Type type = TypeBuilder
+                .newInstance(ApiResponse.class)
+                .addTypeParam(tClass)
+                .build();
+        if (isJSONValid(string)) {
+            return gson.fromJson(string, type);
+        }else {
+            return null;
+        }
+    }
+
+    public <T> ApiResponse<List<T>> convertJson2ApiResponseList(String string, Class<T> tClass) {
+        Type type = TypeBuilder
+                .newInstance(ApiResponse.class)
+                .beginSubType(List.class)
+                .addTypeParam(tClass)
+                .endSubType()
+                .build();
+        if (isJSONValid(string)) {
+            return gson.fromJson(string, type);
+        }else {
+            return null;
+        }
     }
 
     public static boolean isJSONValid(String jsonInString) {
@@ -62,4 +93,21 @@ public class TMCore {
             return false;
         }
     }
+
+    public void makeDelay(int seconds, final Block block) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                block.completion();
+            }
+        }, seconds * 1000);
+    }
+
+    public interface Block {
+        void completion();
+        void failure();
+    }
 }
+
+
