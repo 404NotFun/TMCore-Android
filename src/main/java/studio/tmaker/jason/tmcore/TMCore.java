7232fd75856793af49d3984ac1d0c00f3224a088
Jason Tsai <funnot404@gmail.com>
2017-08-09 17:38:17 +0800

    [New Feature] (TMCore) > MD5 Encrypt

--- src/main/java/studio/tmaker/jason/tmcore/TMCore.java
+++ src/main/java/studio/tmaker/jason/tmcore/TMCore.java
@@ -1,6 +1,8 @@
 package studio.tmaker.jason.tmcore;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -48,6 +50,32 @@ public class TMCore {
         }
 
         return instance;
+    }
+
+    public String hashPassword(String password) {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(password.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 
     public String getVersion(Context context) {
